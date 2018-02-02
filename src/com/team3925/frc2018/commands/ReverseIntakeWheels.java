3db5a225a701302de55dda40b4ff5d76187e4169
AvidhBavkar <avidhbavkar@gmail.com>
2018-02-01 18:33:06 -0800

    Initial Commit

---
+++ src/com/team3925/frc2018/commands/ReverseIntakeWheels.java
@@ -0,0 +1,24 @@
+package com.team3925.frc2018.commands;
+
+import com.team3925.frc2018.subsystems.Intake;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ReverseIntakeWheels extends Command{
+	
+	@Override
+	protected void initialize() {
+		Intake.getInstance().setIntakeRollers(-1);
+	}
+	
+	@Override
+	protected void end() {
+		Intake.getInstance().setIntakeRollers(0);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
