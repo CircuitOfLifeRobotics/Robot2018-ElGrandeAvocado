233be23ee2eef4f98b3d273b9f67ee0db41b73ed
AvidhBavkar <avidhbavkar@gmail.com>
2018-02-19 16:44:54 -0800

    Merged Shuffleboard Test Command

--- src/com/team3925/frc2018/Robot.java
+++ src/com/team3925/frc2018/Robot.java
@@ -1,15 +1,10 @@
 package com.team3925.frc2018;
 
 import com.team3925.frc2018.commands.DriveManual;
-import com.team3925.frc2018.commands.JacisTesting;
-import com.team3925.frc2018.subsystems.Drivetrain;
 import com.team3925.utils.MotionProfileCommand;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import jaci.pathfinder.Waypoint;
 
 public class Robot extends IterativeRobot {
 	DriveManual drive;
