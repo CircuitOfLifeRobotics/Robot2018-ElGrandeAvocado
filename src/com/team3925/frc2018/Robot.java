71c6dc30c4996126731cc094e217ce177736e15c
AvidhBavkar <avidhbavkar@gmail.com>
2018-02-22 19:24:07 -0800

    Prepared for release

--- src/com/team3925/frc2018/Robot.java
+++ src/com/team3925/frc2018/Robot.java
@@ -1,7 +1,6 @@
 package com.team3925.frc2018;
 
 import com.team3925.frc2018.commands.DriveManual;
-import com.team3925.frc2018.commands.RunElevator;
 import com.team3925.frc2018.commands.RunElevatorRaw;
 import com.team3925.frc2018.subsystems.Elevator;
 import com.team3925.utils.MotionProfileCommand;
@@ -12,18 +11,15 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 public class Robot extends IterativeRobot {
 	DriveManual drive;
 	RunElevatorRaw elevateRaw;
-	MotionProfileCommand test;
 
 	@Override
 	public void robotInit() {
 		drive = new DriveManual(OI.getInstance());
 		elevateRaw = new RunElevatorRaw();
-		test = new MotionProfileCommand("testAuto");
 	}
 
 	@Override
 	public void autonomousInit() {
-		test.start();
 	}
 	
 	public void teleopInit() {
