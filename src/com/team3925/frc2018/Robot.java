186072d86c052e26a8894457dd93640979d8e754
AvidhBavkar <avidhbavkar@gmail.com>
2018-02-26 17:13:54 -0800

    Removed non-existent solenoids for Lift Subsystem

--- src/com/team3925/frc2018/Robot.java
+++ src/com/team3925/frc2018/Robot.java
@@ -3,8 +3,6 @@ package com.team3925.frc2018;
 import com.team3925.frc2018.commands.DriveManual;
 import com.team3925.frc2018.commands.RunElevatorRaw;
 import com.team3925.frc2018.subsystems.Elevator;
-import com.team3925.utils.MotionProfileCommand;
-
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
@@ -26,6 +24,10 @@ public class Robot extends IterativeRobot {
 		Elevator.getInstance().zero();
 		drive.start();
 		elevateRaw.start();
+	}
+	
+	@Override
+	public void teleopPeriodic() {
 	}
 	
 	@Override
--- src/com/team3925/frc2018/RobotMap.java
+++ src/com/team3925/frc2018/RobotMap.java
@@ -8,40 +8,40 @@ import com.team3925.utils.CTREControllerFactory;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class RobotMap {
-	
-	public static final class DrivetrainMap{
+
+	public static final class DrivetrainMap {
 		public static final WPI_TalonSRX LEFT_MASTER = CTREControllerFactory.createDefaultTalon(4);
 		public static final WPI_TalonSRX LEFT_SLAVE_A = CTREControllerFactory.createPermanentSlaveTalon(5, LEFT_MASTER);
-		public static final WPI_VictorSPX LEFT_SLAVE_B = CTREControllerFactory.createPermanentSlaveVictor(6, LEFT_MASTER);
-		
+		public static final WPI_VictorSPX LEFT_SLAVE_B = CTREControllerFactory.createPermanentSlaveVictor(6,
+				LEFT_MASTER);
+
 		public static final WPI_TalonSRX RIGHT_MASTER = CTREControllerFactory.createDefaultTalon(1);
-		public static final WPI_TalonSRX RIGHT_SLAVE_A = CTREControllerFactory.createPermanentSlaveTalon(2, RIGHT_MASTER);
-		public static final WPI_VictorSPX RIGHT_SLAVE_B = CTREControllerFactory.createPermanentSlaveVictor(3, RIGHT_MASTER);
-		
+		public static final WPI_TalonSRX RIGHT_SLAVE_A = CTREControllerFactory.createPermanentSlaveTalon(2,
+				RIGHT_MASTER);
+		public static final WPI_VictorSPX RIGHT_SLAVE_B = CTREControllerFactory.createPermanentSlaveVictor(3,
+				RIGHT_MASTER);
+
 		public static final PigeonIMU DRIVETRAIN_IMU = new PigeonIMU(LEFT_SLAVE_A);
-		
+
 		public static final DoubleSolenoid SHIFT_SOLENOID = new DoubleSolenoid(0, 1);
 	}
-	
-	public static final class IntakeMap{
+
+	public static final class IntakeMap {
 		public static final WPI_TalonSRX LEFT_INTAKE = CTREControllerFactory.createDefaultTalon(11);
 		public static final WPI_TalonSRX RIGHT_INTAKE = CTREControllerFactory.createDefaultTalon(12);
-		
+
 		public static final WPI_TalonSRX LIFT_MOTOR = CTREControllerFactory.createDefaultTalon(13);
-		
+
 		public static final DoubleSolenoid GRAB_SOLENOID = new DoubleSolenoid(2, 3);
 	}
 
-	public static final class LiftMap{
+	public static final class LiftMap {
 		public static final WPI_TalonSRX MASTER = CTREControllerFactory.createDefaultTalon(8);
-		public static final WPI_VictorSPX SLAVE  = CTREControllerFactory.createPermanentSlaveVictor(7, MASTER);
+		public static final WPI_VictorSPX SLAVE = CTREControllerFactory.createPermanentSlaveVictor(7, MASTER);
 	}
-	
-	public static final class ElevatorMap{
+
+	public static final class ElevatorMap {
 		public static final WPI_TalonSRX MASTER = CTREControllerFactory.createDefaultTalon(9);
 		public static final WPI_VictorSPX SLAVE = CTREControllerFactory.createPermanentSlaveVictor(10, MASTER);
-		
-		public static final DoubleSolenoid GRABBER_SOLENOID = new DoubleSolenoid(6, 7);
-		public static final DoubleSolenoid SPATULA_SOLENOID = new DoubleSolenoid(4, 5);
 	}
 }
